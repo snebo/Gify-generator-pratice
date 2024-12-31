f2e1bbb2daa97fa2387dc179c52ce86aceb4f42e
stefano <30480337+snebo@users.noreply.github.com>
2024-12-31 21:51:15 +0100

    change promise to async syntax

--- src/index.js
+++ src/index.js
@@ -5,16 +5,23 @@ console.log("Hello World");
 const form = document.querySelector("form");
 const img = document.querySelector(".giphy-image");
 document.addEventListener('DOMContentLoaded', () => {
-    form.addEventListener('submit', (e) => {
+    form.addEventListener('submit', async (e) => {
         e.preventDefault();
         const search_value = form.querySelector("#search").value
         console.log(search_value)
-        fetch(`${BASE_URL}?api_key=${GIPHY_API_KEY}&s=${search_value}&weirdness=4`, { mode: 'cors' })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data.data.images.original.url)
-                console.log(data.data.url)
-                img.src = data.data.images.original.url
-            })
+        const new_img_src = await getGiphyGif(search_value)
+        img.src = new_img_src
+
     })
 })
+async function getGiphyGif(search_value) {
+    try {
+        const response = await fetch(`${BASE_URL}?api_key=${GIPHY_API_KEY}&s=${search_value}&weirdness=4`, { mode: 'cors' })
+        if (!response.ok) throw new Error('something went wrong')
+
+        const data = await response.json()
+        return data.data.images.original.url
+    } catch (error) {
+        console.log(error)
+    }
+}
